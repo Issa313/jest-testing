db7195bc4535c288556438e08f6c6fb401e83f19
Issa313 <isalee313@gmail.com>
2024-03-12 13:45:21 -0400

    added first fizzbuzz test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
-  export function add(a, b) {
-      return 0;
-  }
+export function add(a, b) {
+  return a + b;
+}
+
+export function fizzbuzz(a) {
+  return a;
+}
+export function subtract(a, b) {
+  return a - b;
+}
