30502ae0ecc9d5a8efd2c897e39e459980d6f707
Issa313 <isalee313@gmail.com>
2024-04-15 13:50:29 -0400

    added new code

--- src/app.test.js
+++ src/app.test.js
@@ -69,4 +69,33 @@ describe("my test suite", () => {
     const result = fizzbuzzTo(2);
     expect(result).toEqual([1, 2]);
   });
+
+  it("fizzbuzzz To list of 3", () => {
+    const result = fizzbuzzTo(3);
+    expect(result).toEqual([1, 2, "fizz"]);
+  });
+  it("fizzbuzzz To list of 5", () => {
+    const result = fizzbuzzTo(5);
+    expect(result).toEqual([1, 2, "fizz", 4, "buzz"]);
+  });
+  it("fizzbuzzz To list of 15", () => {
+    const result = fizzbuzzTo(15);
+    expect(result).toEqual([
+      1,
+      2,
+      "fizz",
+      4,
+      "buzz",
+      "fizz",
+      7,
+      8,
+      "fizz",
+      "buzz",
+      11,
+      "fizz",
+      13,
+      14,
+      "fizzbuzz",
+    ]);
+  });
 });
